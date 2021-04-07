b52a776459ad1d3f16705bb385f1c893d37f9938
krsinghshubham <47324260+krsinghshubham@users.noreply.github.com>
2021-04-07 11:40:10 +0530

    adding admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :orders
-  validates :first_name, format: { with:  /\A[a-zA-Z0-9 ]+\z/ }
+  # validates :first_name, format: { with:  /\A[a-zA-Z0-9 ]+\z/ }
 end
